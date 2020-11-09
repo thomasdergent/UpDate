2063a6e3a2a609f7b071f6378cdd975cc9a6f205
thomasdergent <thomas.der@hotmail.com>
2020-11-09 16:55:42 +0100

    Creation of models, guards, services and views

---
+++ src/app/views/addarticle/addarticle.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddarticleComponent } from './addarticle/addarticle.component';
+
+
+
+@NgModule({
+  declarations: [AddarticleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AddarticleModule { }
---
+++ src/app/views/addarticle/addarticle/addarticle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addarticle',
+  templateUrl: './addarticle.component.html',
+  styleUrls: ['./addarticle.component.scss']
+})
+export class AddarticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
